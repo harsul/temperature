c826dfe4c74b53a323bd37766e2acc1c7ef45be7
harsul <48764315+harsul@users.noreply.github.com>
2019-04-15 11:54:48 +0300

    Add files via upload

--- Project_Sensors.py
+++ Project_Sensors.py
@@ -21,40 +21,40 @@ GPIO.setup(soil, GPIO.IN)
 GPIO.setup(ldr,GPIO.IN)
 
 while True:
+    print("Last valid input: " + str(datetime.datetime.now()))
 	#temperature and humidity sensor readings
     result = temp_hum.read()
     if result.is_valid():
-		temperature=result.temperature
-		humidity=result.humidity
-        print("Last valid input: " + str(datetime.datetime.now()))
+        temperature = result.temperature
+        humidity = result.humidity
         print("Temperature: %d C" % temperature)
         print("Humidity: %d %%" % humidity)
 		
 	#soil measurement sensor readings
-	if GPIO.input(soil):
+    if GPIO.input(soil):
 	#'No water' = 1/True (sensor's microcontroller light is off).
-		soil=0
-		print("Soil: No water detected")
-	else:
-		soil=1
+        soil=0
+        print("Soil: No water detected")
+    else:
+        soil=1
     #'Water' = 0/False (microcontroller light is on).
-		print("Soil: Water detected!")
+        print("Soil: Water detected!")
 	
 	#rain sensor readings
-	if not no_rain.is_active:
-		rain=1
+    if not no_rain.is_active:
+        rain=1
         print("Rain: Rain detected")
-	else:
-		rain=0
-		print("Rain: No rain detected")
+    else:
+        rain=0
+        print("Rain: No rain detected")
         
-	#ldr sensor readings
-	if GPIO.input(ldr):
-		light=1
-		print ("Light: Yes")
-	else:
-		light=0
-		print ("Light: No")
+	#ldr sensor reading
+    if GPIO.input(ldr):
+        light=1
+        print ("Light: Yes")
+    else:
+        light=0
+        print ("Light: No")
 		
     time.sleep(2)
 	
